f757739f4b2c715e93b798690c34a5e8e5a9ba49
Aleksey Pivchenkov <aleksey_pivchenkov@epam.com>
2022-07-08 10:55:52 +0000

    Ready

--- functions.sh
+++ functions.sh
@@ -1,3 +1,39 @@
 #!/bin/bash
 
 # Put your code here
+pow () {
+    result=$(($1**$2))
+    echo $result
+}
+
+shortest () {
+    total=$#
+    arguments=( $@ )
+    shortest_string_lenght=${#arguments[0]}
+    shortest_string_number=0
+
+    for (( i=0; i<$total; i++ ))
+    do
+        current_string=${arguments[$i]}
+        #echo "$current_string ${#current_string}"
+        #echo "------------------------------------------"
+        if [[ ${#current_string} < $shortest_string_lenght ]]
+        then
+            shortest_string_lenght=${#current_string}
+        fi
+    done
+
+    for (( i=0; i<$total; i++ ))
+    do
+        current_string=${arguments[$i]}
+        if [[ ${#current_string} == $shortest_string_lenght ]]
+        then
+            echo $current_string
+        fi
+    done
+}
+
+print_log () {
+    now=$(date +%F" "%R)
+    echo "["$now"]" $1
+}
